851569dd21475f4442a9060392fc5cb7798a211f
Dare Samuel <dawakesam@gmail.com>
2018-05-08 00:13:36 +0100

    Static files Set

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
+//Iitialize Express constructor
 const app = express();
+
+//Port
 let port = process.env.PORT || 3000;
+
+//Serving Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up Handlebars Engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('Hello');
+    const title = 'Home';
+    res.render('home', {
+        title: title
+    });
+});
+
+app.get('/about', (req, res) => {
+    const title = 'About';
+    res.render('about', {
+        title: title
+    });
 });
 
 
